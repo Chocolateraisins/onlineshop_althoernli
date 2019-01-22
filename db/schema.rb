c76de002aa86cdb7a46638c31b3f19d565cd3ea4
Stephen Schuez <stephen.schuez@gmail.com>
2019-01-22 18:46:01 +0100

    set up inital onlineshop with Teddies

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_22_075455) do
+ActiveRecord::Schema.define(version: 2019_01_22_171156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teddies", force: :cascade do |t|
+    t.string "sku"
+    t.string "name"
+    t.bigint "category_id"
+    t.string "photo_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "price_cents", default: 0, null: false
+    t.index ["category_id"], name: "index_teddies_on_category_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +40,10 @@ ActiveRecord::Schema.define(version: 2019_01_22_075455) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "teddies", "categories"
 end
